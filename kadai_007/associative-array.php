4f600f0b8a94584875c8c098e016dd97db9793ef
KojiDann <dannokouji@gmail.com>
2025-10-04 14:25:05 +1000

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html land="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP basic</title>
+</head>
+
+<body>
+    <p>
+        <?php
+
+        $grocery = ['name'=>'onion', 'price'=>200, 'weight'=>160];
+
+        print_r($grocery);
+
+        ?>
+    </p>
+
+
+</body>
+
+</html>
