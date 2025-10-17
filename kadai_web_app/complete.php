36ebf111bfe832ed820b328bf69eb649ff72718d
KojiDann <dannokouji@gmail.com>
2025-10-18 10:25:58 +1100

    Add files via upload

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html land="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP basic</title>
+</head>
+
+<body>
+    <h2>登録が完了しました。</h2>
+    <p>社員情報がデータベースに保存されました。</p>
+    <button id="back-btn" onclick="location.href='form.php';">戻る</button>
+    
+</body>
+
+</html>
